7146035c16a324de28c3f5348fd86d43ccf0528b
DimDim <dimaasad1385@gmail.com>
2026-06-12 17:03:27 +0330

    Initial project setup

---
+++ src/game/GameMain.java
@@ -0,0 +1,7 @@
+package game;
+import javax.swing.*;
+
+public class GameMain {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/model/boss/Boss.java
@@ -0,0 +1,4 @@
+package model.boss;
+
+public class Boss {
+}
